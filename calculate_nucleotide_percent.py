358def772ecba7183a061e902fe7ee92a9ea849d
reismgadsden <rgadsden287@gmail.com>
2022-02-23 14:20:31 -0500

    First commit

---
+++ calculate_nucleotide_percent.py
@@ -0,0 +1,18 @@
+"""
+Calculate the total percents of nucleotides between
+two sequences in order to compare two sequences.
+
+author: Reis Gadsden
+version: 23/02/22
+
+class: CS-5531 @ Appalchian State University
+instructor: Mohammad Mohebbi
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
